1f7b25bc9cc669ce5a7ce59c2966f4c6c73db22e
Thane <thabay0606@students.maquoketaschools.org>
2026-01-16 12:51:05 -0600

    Fourth commit.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Put your code here
-function totalAmount(...) {
+const display = getElementById("")
+
+const itemOne = getElementById("add-cost-34");
+
+const itemTwo = getElementById("add-cost-60");
+
+const itemThree = getElementById("add-cost-70");
+
+
+function totalAmount() {
 
 }
